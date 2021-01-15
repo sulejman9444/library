a870f6e3f81d89b6a9a9c437bfda5ac0af979b78
Sulejman Delic <sulejman.delic@virginpulse.com>
2021-01-15 06:48:06 -0500

    fix: 🐛 test
    test

--- projects/components-lib/src/lib/components-lib.module.ts
+++ projects/components-lib/src/lib/components-lib.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { ComponentsLibComponent } from './components-lib.component';
+import { FooComponent } from '../foundations/foo.component';
 
 
 
 @NgModule({
-  declarations: [ComponentsLibComponent],
+  declarations: [ComponentsLibComponent, FooComponent],
   imports: [
   ],
-  exports: [ComponentsLibComponent]
+  exports: [ComponentsLibComponent, FooComponent]
 })
 export class ComponentsLibModule { }
