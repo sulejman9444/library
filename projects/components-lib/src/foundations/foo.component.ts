e7e2bd93e2b3927f05606c194a2acbfe38b8e543
Sulejman Delic <sulejman.delic@virginpulse.com>
2021-01-15 06:20:51 -0500

    feat: 🎸 foo
    test

---
+++ projects/components-lib/src/foundations/foo.component.ts
@@ -0,0 +1,47 @@
+// src/app/components/task.component.ts
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+@Component({
+  selector: 'app-foo',
+  template: `
+    <div class="list-item {{ task?.state }}">
+      <label class="checkbox">
+        <input
+          type="checkbox"
+          [defaultChecked]="task?.state === 'TASK_ARCHIVED'"
+          disabled="true"
+          name="checked"
+        />
+        <span class="checkbox-custom" (click)="onArchive(task.id)"></span>
+      </label>
+      <div class="title">
+        <input type="text" [value]="task?.title" readonly="true" placeholder="Input title" />
+      </div>
+      <div class="actions">
+        <a *ngIf="task?.state !== 'TASK_ARCHIVED'" (click)="onPin(task.id)">
+          <span class="icon-star"></span>
+        </a>
+      </div>
+    </div>
+  `,
+})
+export class FooComponent implements OnInit {
+  title = '';
+  @Input() task = {};
+
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output() onPinTask: EventEmitter<any> = new EventEmitter();
+
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output() onArchiveTask: EventEmitter<any> = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() { }
+
+  onPin(id: any) {
+    this.onPinTask.emit(id);
+  }
+  onArchive(id: any) {
+    this.onArchiveTask.emit(id);
+  }
+}
